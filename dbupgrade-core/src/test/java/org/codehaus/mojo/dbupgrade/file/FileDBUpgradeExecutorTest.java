7e495b00de9ef3a7333362a62bfe2557145d1001
Dan Tran <dantran@gmail.com>
2010-02-04 20:37:17 +0000

    clean up poms
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/dbupgrade@11802 52ab4f32-60fc-0310-b215-8acea882cd1b

--- dbupgrade-core/src/test/java/org/codehaus/mojo/dbupgrade/file/FileDBUpgradeExecutorTest.java
+++ dbupgrade-core/src/test/java/org/codehaus/mojo/dbupgrade/file/FileDBUpgradeExecutorTest.java
@@ -7,7 +7,7 @@ import org.codehaus.mojo.dbupgrade.DBUpgradeLifecycle;
 import junit.framework.TestCase;
 
 /*
- * Copyright 2000-2006 The Apache Software Foundation
+ * Copyright 2000-20010 The Apache Software Foundation
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
