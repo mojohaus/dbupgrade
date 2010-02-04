f937aa1fb7b194fabb6c6d48c4a51e48489f84fe
Dan Tran <dantran@gmail.com>
2010-02-04 20:25:57 +0000

    add lincense headers
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/dbupgrade@11801 52ab4f32-60fc-0310-b215-8acea882cd1b

--- dbupgrade-core/src/main/java/org/codehaus/mojo/dbupgrade/generic/GenericDBUpgradeLifecycle.java
+++ dbupgrade-core/src/main/java/org/codehaus/mojo/dbupgrade/generic/GenericDBUpgradeLifecycle.java
@@ -16,6 +16,21 @@ import org.codehaus.mojo.dbupgrade.DBUpgradeLifecycle;
 import org.codehaus.mojo.dbupgrade.Util;
 import org.codehaus.mojo.dbupgrade.sqlexec.DefaultSQLExec;
 
+/*
+ * Copyright 2000-20010 The Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
 /**
  * Incremental Database upgrade implementation, capable of understanding both java and sql format
  * Original source is from http://code.google.com/p/dbmigrate
