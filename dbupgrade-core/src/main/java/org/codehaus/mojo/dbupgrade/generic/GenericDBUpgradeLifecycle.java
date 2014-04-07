368ec6163174d37868f2b5e7c26a69b85ae748b6
Dan Tran <dantran@gmail.com>
2014-04-07 19:28:29 +0000

    Update license headers
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/dbupgrade@19634 52ab4f32-60fc-0310-b215-8acea882cd1b

--- dbupgrade-core/src/main/java/org/codehaus/mojo/dbupgrade/generic/GenericDBUpgradeLifecycle.java
+++ dbupgrade-core/src/main/java/org/codehaus/mojo/dbupgrade/generic/GenericDBUpgradeLifecycle.java
@@ -1,5 +1,24 @@
 package org.codehaus.mojo.dbupgrade.generic;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -14,20 +33,6 @@ import org.codehaus.mojo.dbupgrade.sqlexec.DefaultSQLExec;
 import org.codehaus.mojo.dbupgrade.sqlexec.SQLUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-/*
- * Copyright 2000-2010 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
 
 /**
  * This class hooks up your global pre-upgrade, incremental upgrades, and finally global post-upgrade using both java and SQL
